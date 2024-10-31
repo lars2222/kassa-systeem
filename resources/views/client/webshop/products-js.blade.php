d357a767ba4ec9006514cc76d86d0f93d1467987
lars2222 <larsdeboer22@gmail.com>
2024-10-31 08:59:04 +0100

    changed blades client side
    changed blades client side

--- resources/views/client/webshop/products-js.blade.php
+++ resources/views/client/webshop/products-js.blade.php
@@ -13,5 +13,27 @@
                 })
                 .catch(error => console.error('Error:', error));
         });
+
+        function openForm() {
+            document.getElementById("product-info").style.display = "block";
+        }
+
+        function closeForm() {
+            document.getElementById("product-info").style.display = "none";
+        }
+
+        function toggleProductInfo(button) {
+            const productInfo = button.closest('.card-body').querySelector('.product-info');
+            if (productInfo.style.display === "none") {
+                productInfo.style.display = "block";
+            } else {
+                productInfo.style.display = "none";
+            }
+        }
+
+        // Zet de functies in de globale scope
+        window.openForm = openForm;
+        window.closeForm = closeForm;
+        window.toggleProductInfo = toggleProductInfo;
     });
 </script>
